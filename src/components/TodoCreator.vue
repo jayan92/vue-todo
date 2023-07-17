91a8808df11ab5da0782ebf89fb1026f933ecaa2
Jayan Chinthaka <mail4jayanchinthaka@gmail.com>
2023-07-17 20:05:07 +0530

    first commit

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="input-wrap" :class="{ 'input-err' : todo.invalid }">
+        <input type="text" v-model="todo.todo" />
+        <TodoButton @click="createTodo" />
+    </div>
+    <p v-show="todo.invalid" class="err-msg">{{ todo.errMsg }}</p>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+import TodoButton from "./TodoButton.vue";
+
+const emit = defineEmits(['create-todo'])
+
+const todo = reactive({
+    todo: '',
+    invalid: false,
+    errMsg: ''
+});
+
+const createTodo = () => {
+    todo.errMsg = ''
+    todo.invalid = false
+
+    if(todo.todo !== '') {
+        emit('create-todo', todo.todo)
+        todo.todo = ''
+        return
+    }
+    todo.invalid = true
+    todo.errMsg = 'Todo value cannot be empty!'
+}
+</script>
+
+<style lang="scss" scoped>
+.input-wrap {
+    display: flex;
+    transition: 250ms ease;
+    border: 2px solid #41b080;
+    &.input-err {
+        border-color: red;
+    }
+    &:focus-within {
+        box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1), 0 -2px 4px -2px rgba(0 0 0 / 0.1);
+    }
+    input {
+        width: 100%;
+        padding: 8px 6px;
+        border: none;
+        &:focus {
+            outline: none;
+        }
+    }
+}
+.err-msg {
+    margin-top: 6px;
+    font-size: 12px;
+    text-align: center;
+    color: red;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1 class="title">Vue Todos</h1>
+    <p>
+      Vue Todos is a CRUD Todo tracking application created by Vue 3.
+      <br><br>
+      Track your daily task's using this todo tracker with abilities to save, complete, edit & delete todos.
+      <br><br>
+      Feel free to add on to this application with functionalities such as the ability to drag and reorder todos, animate creation/deletion of todos or even create this into a full stack application with the ability to have different lists for different days of tracking todos!
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    padding: 0px 80px;
+
+    .title {
+      margin-top: 60px;
+      margin-bottom: 40px;
+    }
+  }
+}
+</style>
